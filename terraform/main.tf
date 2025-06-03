54ce5eecea2cf6ab7217a96fab8957acc79e1add
Atul Sharma <atulvsharma@gmail.com>
2025-06-03 15:00:23 -0400

    Updated the main.tf with backend resources and updated the bootstrap.sh

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,22 +4,14 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-<<<<<<< HEAD
-    storage_account_name = "yourstorageacct"
-=======
-    storage_account_name = "devtfstorageacct"
->>>>>>> 071a9d8bb3de5969465e7ca518af0e3827386380
-    container_name       = "tfstate"
+    resource_group_name  = "aksdev-tfstate-rg"
+    storage_account_name = "aksdevstracct"
+    container_name       = "aksdevtfstate"
     key                  = "terraform.tfstate"
   }
 }
 
-<<<<<<< HEAD
 resource "azurerm_resource_group" "example" {
-=======
-resource "azurerm_resource_group" "rgp_name" {
->>>>>>> 071a9d8bb3de5969465e7ca518af0e3827386380
   name     = var.rg_name
   location = var.location
 }
