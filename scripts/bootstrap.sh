ac8397b7c028ae07eb979fe89c25aa42303fbdd3
Atul Sharma <atulvsharma@gmail.com>
2025-06-10 10:58:42 -0400

    Made additional changes

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set -e
 
-ROOT_DIR=$(cd "$(dirname "$0")/.." && pwd)
-TF_DIR="$ROOT_DIR/terraform-manifests"
+#ROOT_DIR=$(cd "$(dirname "$0")/.." && pwd)
+#TF_DIR="$ROOT_DIR/terraform-manifests"
 
 echo "Root directory: $ROOT_DIR"
 echo "Terraform directory: $TF_DIR"
 echo "Initializing Terraform backend for all environments..."
 
-for env in dev qa prod stage; do
+cd terraform-manifests
+
+#for env in dev qa prod stage; do
   echo "Applying Terraform backend for $env"
 
-  cd "$TF_DIR"
-  echo "Current directory: $(pwd)"
-  echo "Looking for: $TF_DIR/$env.tfvars"
+  #cd "$TF_DIR"
+  #echo "Current directory: $(pwd)"
+  #echo "Looking for: $TF_DIR/$env.tfvars"
 
   #terraform init -backend-config=backend.tfvars
   terraform init
-  terraform apply -var-file="$TF_DIR/$env.tfvars" -auto-approve
-done
+  #terraform apply -var-file="$TF_DIR/$env.tfvars" -auto-approve
+  terraform apply -var-file="$env.tfvars" -auto-approve
+#done
--- terraform-manifests/backend.tfvars
+++
@@ -1,4 +0,0 @@
-resource_group_name = "$BACKEND_RG"
-storage_account_name = "$BACKEND_STORAGE"
-container_name       = "$CONTAINER_NAME"
-key                  = "terraform.tfstate"
