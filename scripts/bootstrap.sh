ce96c9ed6bd147a6109a414c08eb09fe378946f1
Atul Sharma <atulvsharma@gmail.com>
2025-06-10 09:41:09 -0400

    Updated the bootstrap.sh file

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Initializing Terraform backend for all environments..."
+ROOT_DIR=$(pwd)
+TF_DIR="$ROOT_DIR/terraform-manifests"
 
-cd terraform-manifests
+echo "Initializing Terraform backend for all environments..."
 
 for env in dev qa prod stage; do
   echo "Applying Terraform backend for $env"
+  cd "$TF_DIR"
   #terraform init -backend-config=backend.tfvars
   terraform init
   terraform apply -var-file="$env.tfvars" -auto-approve
