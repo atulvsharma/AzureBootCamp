034e676b507949252d0d16847aa0ed0cb4480cca
Atul Sharma <atulvsharma@gmail.com>
2025-06-10 15:43:09 -0400

    Updated the backend.tf

--- terraform-manifests/backend/backend.tf
+++ terraform-manifests/backend/backend.tf
@@ -16,10 +16,11 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = azurerm_resource_group.rg.name
-    storage_account_name = azurerm_storage_account.storage.name
-    container_name       = azurerm_storage_container.container.name
-    key                  = "terraform.tfstate"
+    #resource_group_name  = azurerm_resource_group.rg.name
+    #storage_account_name = azurerm_storage_account.storage.name
+    #container_name       = azurerm_storage_container.container.name
+    #key                  = "terraform.tfstate"
+    # Backend block intentionally left empty here; values are provided via CLI flags or .tfvars
   }
 }
 
