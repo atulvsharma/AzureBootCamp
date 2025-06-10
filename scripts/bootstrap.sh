1b69d017d66111e38bb0fca66d1d884f82e869b9
Atul Sharma <atulvsharma@gmail.com>
2025-06-10 15:18:48 -0400

    Made additional changes to deploy.yml file

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+export ARM_CLIENT_ID="${AZURE_CLIENT_ID}"
+export ARM_CLIENT_SECRET="${AZURE_CLIENT_SECRET}"
+export ARM_SUBSCRIPTION_ID="${AZURE_SUBSCRIPTION_ID}"
+export ARM_TENANT_ID="${AZURE_TENANT_ID}"
+
+
 if [ -z "$1" ]; then
   echo "Usage: $0 <environment>"
   exit 1
