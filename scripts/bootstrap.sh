8351acdbbd7ac94a570149da20b2972bb3989f41
Atul Sharma <atulvsharma@gmail.com>
2025-06-10 09:31:52 -0400

    Corrected the path for dev.tfvars file

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Initializing Terraform backend for all environments..."
 
-cd terraform-manifests
+#cd terraform-manifests
 
 for env in dev qa prod stage; do
   echo "Applying Terraform backend for $env"
