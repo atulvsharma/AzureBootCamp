8931762ac8a4cf0971233ad7a9418923c527561b
Atul Sharma <atulvsharma@gmail.com>
2025-06-10 15:59:05 -0400

    Again updated deploy.yml and bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,6 +11,13 @@ export ARM_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}
 export ARM_TENANT_ID=${AZURE_TENANT_ID}
 
 echo "Current directory: $(pwd)"
-terraform init
+#terraform init
+terraform init \
+  -backend-config="resource_group_name=rg-dev-tfstate" \
+  -backend-config="storage_account_name=stdevtfstate001" \
+  -backend-config="container_name=tfstate" \
+  -backend-config="key=dev.tfstate"
+echo "Terraform initialized successfully with backend configuration."
+
 terraform apply -var-file="../${env}.tfvars" -auto-approve
 echo "Terraform backend resources provisioned successfully for environment: $env"
