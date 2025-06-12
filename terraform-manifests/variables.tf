030864c568876154e19df9c97e74cc58333e7083
Atul Sharma <atulvsharma@gmail.com>
2025-06-12 19:30:16 -0400

    Removed duplicate environment variable from variables.tf

--- terraform-manifests/variables.tf
+++ terraform-manifests/variables.tf
@@ -14,10 +14,10 @@ variable "resource_group_name" {
 }
 
 
-variable "environment" {
-  description = "The deployment environment name (dev, qa, prod, stage)"
-  type        = string
-}
+#variable "environment" {
+#  description = "The deployment environment name (dev, qa, prod, stage)"
+#  type        = string
+#}
 
 variable "location" {
   description = "Azure region to deploy resources"
