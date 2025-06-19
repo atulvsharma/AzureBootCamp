f1a8c42ea1e93c837531938bfefc16b0ce453789
Atul Sharma <atulvsharma@gmail.com>
2025-06-19 15:12:00 -0400

    Added ssh keys to git and updated deploy.yml

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,4 +1,8 @@
-variable "resource_group_name" {}
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name for the bastion host"
+}
+
 
 variable "ssh_public_key_path" {}
 variable "admin_username" {}
@@ -10,4 +14,24 @@ variable "env" {
 variable "location" {
   description = "Azure region"
   type        = string
+}
+
+variable "environment" {
+  description = "The environment name (e.g., dev, qa)"
+  type        = string
+}
+
+variable "backend_storage" {
+  description = "The backend storage account name"
+  type        = string
+}
+
+variable "bastion_subnet_id" {
+  type        = string
+  description = "Subnet ID for Bastion"
+}
+
+variable "bastion_public_ip_id" {
+  type        = string
+  description = "Public IP ID for Bastion"
 }
